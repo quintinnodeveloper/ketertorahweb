651339314487486aefee14dc5bde844b3d631216
Quintinno Developer <quintinno.developer@gmail.com>
2024-11-24 08:33:47 -0300

    🔵 feature: Implementar etapas de cadastro no modal de cadastro de associados

--- resources/scripts/associado.page.js
+++ resources/scripts/associado.page.js
@@ -2,8 +2,6 @@ const abrirModal = document.getElementById("abrir-modal");
 const dialog = document.getElementById("dialog");
 const sombreamento = document.getElementById("sombreamento");
 
-sombreamento.style.display = "block";
-
 abrirModal.addEventListener("click", function() {
     sombreamento.style.display = "block";
     dialog.show();
