d7608c73264ee548e959462e58a50d06d080655d
Quintinno Developer <quintinno.developer@gmail.com>
2024-11-26 16:33:43 -0300

    🔵 feature: Apresentar mensagem de associao não encontrado quando não houver correspondência no filtro por nomes

--- component/toast-component/toast.component.js
+++ component/toast-component/toast.component.js
@@ -1,5 +1,6 @@
 const toast = document.getElementById("toast");
 const barraProgresso = document.getElementById("barraProgresso");
+const mensagemToast = document.getElementById("mensagemToast");
 
 document.getElementById("fecharToast").addEventListener("click", function() {
     toast.classList.remove("show");
@@ -7,11 +8,42 @@ document.getElementById("fecharToast").addEventListener("click", function() {
 
 export function apresentarToastSuccess() {
     toast.classList.add("show");
+    toast.classList.add("success");
     barraProgresso.style.width = "0%";
+    mensagemToast.textContent = "Operação Realizada com Sucesso!";
     setTimeout(() => {
+        barraProgresso.classList.add("success");
         barraProgresso.style.width = "100%";
     }, 25);
     setTimeout(() => {
         toast.classList.remove("show");
     }, 3000);
 }
+
+export function apresentarToastDanger() {
+    toast.classList.add("show");
+    toast.classList.add("danger");
+    barraProgresso.style.width = "0%";
+    setTimeout(() => {
+        barraProgresso.classList.add("danger");
+        barraProgresso.style.width = "100%";
+    }, 25);
+    setTimeout(() => {
+        toast.classList.remove("show");
+    }, 3000);
+}
+
+export function apresentarToastWarning() {
+    toast.classList.add("show");
+    toast.classList.add("warning");
+    barraProgresso.style.width = "0%";
+    setTimeout(() => {
+        barraProgresso.classList.add("warning");
+        barraProgresso.style.width = "100%";
+    }, 25);
+    setTimeout(() => {
+        toast.classList.remove("show");
+    }, 3000);
+}
+
+apresentarToastSuccess();
