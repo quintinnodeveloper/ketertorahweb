7db424c9f34530f4c8e5d6c559250a160c5dbc82
Quintinno Developer <quintinno.developer@gmail.com>
2024-11-27 23:13:25 -0300

    🔵 feature: Implementar utilitário de máscara para CEP

--- modules/utility/mascaras.utility.js
+++ modules/utility/mascaras.utility.js
@@ -1,7 +1,9 @@
-export function aplicarMascaraCep(cep) {
+export function aplicarMascaraCEP(cep) {
     let valor = cep.replace(/\D/g, '');
-    if (valor.length > 5) {
-        valor = valor.slice(0, 5) + '-' + valor.slice(5, 8);
+    if (valor.length > 2 && valor.length <= 5) {
+        valor = valor.slice(0, 2) + '.' + valor.slice(2);
+    } else if (valor.length > 5) {
+        valor = valor.slice(0, 2) + '.' + valor.slice(2, 5) + '-' + valor.slice(5, 8);
     }
     return valor;
 }
