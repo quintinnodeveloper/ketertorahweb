7c8c62e21f17422846d93c7343524884d378811b
Quintinno Developer <quintinno.developer@gmail.com>
2024-12-01 18:38:02 -0300

    🔵 feature: Implementar submenu de preferências do usuário

--- component/nav-component/nav.component.js
+++ component/nav-component/nav.component.js
@@ -4,3 +4,11 @@ const nomeFuncao = document.getElementById("nomeFuncao");
 nomeFuncao.addEventListener("click", function() {
     menuPreferenciaUsuario.classList.toggle("active");
 });
+
+document.addEventListener("click", function(event) {
+    const menuPreferenciaUsuario = document.getElementById("menuPreferenciaUsuario");
+    const nomeFuncao = document.getElementById("nomeFuncao");
+    if (!nomeFuncao.contains(event.target)) {
+        menuPreferenciaUsuario.classList.remove("active");
+    }
+});
