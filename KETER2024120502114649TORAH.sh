f75b0c7282a6986df8d4d9d9d19b80dc887c70c9
Quintinno Developer <quintinno.developer@gmail.com>
2024-12-06 09:59:56 -0300

    🔵 feature: Configurar live server para subir projeto

--- KETER2024120502114649TORAH.sh
+++ KETER2024120502114649TORAH.sh
@@ -1,5 +1,15 @@
 # Demanda:                KETER2024120502114649TORAH
-# Objetivo:               Configurar NodeJS no projeto VanillaJS
+# Objetivo:               Configurar NodeJS no projeto VanillaJS (Ubuntu)
 # Pré requisitos:         Ter o NodeJS instalado
+
+sudo apt-get install nala -y
+
+sudo nala install nodejs -y
+sudo nala install npm -y
+
+sudo npm install -g live-server
+
+live-server
+
 npm init -y
-npm install lite-server --save-dev
+sudo npm install lite-server --save-dev
